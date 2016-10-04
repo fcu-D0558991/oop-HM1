45a23e91c89c82125f9728641479b01779ce03d8
LAPTOP-IFP7KSQ2\WDJ <961575596@qq.com>
2016-10-04 15:55:21 +0800

    homework1

---
+++ src/com/exercise/exe1.java
@@ -0,0 +1,10 @@
+package com.exercise;
+
+public class exe1 {
+	String it="her";
+    public static void main(String[] args) {
+    	
+    	String lyriccs="Let it go!Let it go!Cannot hold it back anymore";
+    	System.out.println(lyriccs);
+	}	
+}
